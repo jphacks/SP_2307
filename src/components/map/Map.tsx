c9736100e18e1fd1cb7afe772c1d07798c3b1c53
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2023-10-29 10:34:23 +0900

    [feat] 送信時に送信完了したことを通知する

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import styles from './Map.module.css';
 //import L from "leaflet";
@@ -6,25 +6,16 @@ import styles from './Map.module.css';
 import { useState } from 'react';
 import { useComments } from '../../hooks/useComments';
 import { geopointToArray } from '../../utils/geopoint';
-import { useGeopoint } from '../../hooks/useGeopoint';
-
-type Message = {
-  position: [number, number];
-  content: string;
-  images?: string;
-  id: number;
-}
-
 
 export const Map: React.FC = () => {  // 緯度軽度
   const position = [43.062, 141.3543];
   // 初期マップズームレベル
   const zoom = 15;
-  const [ successDisplay, setSuccessDisplay ] = useState(false);
+  const [successDisplay, setSuccessDisplay] = useState(false);
 
   const [content, setContent] = useState<string>("");
   const { comments, addComment } = useComments();
-  /*
+
   const onAddCommentSuccesss = () => {
     setSuccessDisplay(true);
   }
@@ -32,18 +23,9 @@ export const Map: React.FC = () => {  // 緯度軽度
   if (successDisplay) {
     setTimeout(() => {
       setSuccessDisplay(false);
-    }, 1000);
+    }, 7000);
   }
 
-  useEffect(() => {
-    return () => {
-      
-    }
-  }, []);
-
-  */
-
-  
   return (
     <div className={styles.map}>
       <MapContainer center={position} zoom={zoom}>
@@ -53,7 +35,7 @@ export const Map: React.FC = () => {  // 緯度軽度
         />
 
         {
-          comments.map(msg => {
+          useMemo(() => comments.map(msg => {
             if (!msg.position) return null;
             return (
               <Marker position={geopointToArray(msg.position)} key={msg.id}>
@@ -62,26 +44,39 @@ export const Map: React.FC = () => {  // 緯度軽度
                 </Popup>
               </Marker>
             )
-          })
+          }), [comments])
         }
       </MapContainer>
 
       <div className={styles.footer}>
-        <div className={styles.sender}>
-          <input className={styles.textbox}
-            value={content}
-            onInput={(e) => setContent(e.currentTarget.value)}
-          />
-          <button className={styles.button} onClick={() => {
-            addComment(content);
-            setContent("");
-          }}>
-            <span className="material-symbols-outlined">
-              send
-            </span>
-          </button>
-        </div>
+        <div className={styles.margin}>
+          {
+            useMemo(() => successDisplay ? (
+              <div className="alert alert-success">
+                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>メッセージの送信が完了しました。</span>
+              </div>
+            ) : null, [successDisplay])}
+          {
+            useMemo(() => (
+              <div className={styles.sender}>
+                <input className={styles.textbox}
+                  value={content}
+                  onInput={(e) => setContent(e.currentTarget.value)}
+                />
+                <button className={styles.button} onClick={() => {
+                  addComment(content);
+                  setContent("");
+                }}>
+                  <span className="material-symbols-outlined">
+                    send
+                  </span>
+                </button>
+              </div>
+            ), [content])}
+          </div>
       </div>
+
     </div>
   )
 };
